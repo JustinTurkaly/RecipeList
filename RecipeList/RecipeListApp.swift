f78a77974e422a15235ac314816352f66b801899
JustinTurkaly <jturkaly18@gmail.com>
2023-01-28 12:49:13 -0600

    Initial Commit

---
+++ RecipeList/RecipeListApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeListApp.swift
+//  RecipeList
+//
+//  Created by Justin Turkaly on 1/28/23.
+//
+
+import SwiftUI
+
+@main
+struct RecipeListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
